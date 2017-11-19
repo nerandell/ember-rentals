661b2848e22dd926bbe409faf794202350f0b4e4
Ankit Chandawala <ankit.chandawala@kayako.com>
2017-11-19 12:39:02 +0530

    Add mirage as dependency

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,18 +1,36 @@
-import { test } from 'qunit';
+import {test} from 'qunit';
 import moduleForAcceptance from 'ember-rentals/tests/helpers/module-for-acceptance';
 
 moduleForAcceptance('Acceptance | list rentals');
 
 test('should show rentals as the home page', function (assert) {
+  visit('/');
+  andThen(function () {
+    assert.equal(currentURL(), '/rentals', 'should redirect to rentals page');
+  })
 });
 
 test('should link to information about the company.', function (assert) {
+  visit('/')
+  click('a:contains("About")');
+  andThen(function () {
+    assert.equal(currentURL(), '/about', 'should link to about page')
+  })
 });
 
 test('should link to contact information.', function (assert) {
+  visit('/');
+  click('a:contains("Contact")');
+  andThen(function () {
+    assert.equal(currentURL(), '/contact', 'should link to contact page')
+  })
 });
 
 test('should list available rentals.', function (assert) {
+  visit('/');
+  andThen(function () {
+    assert.equal(find('.listing').length, 3, 'should see 3 listings')
+  });
 });
 
 test('should filter the list of rentals by city.', function (assert) {
